ffb2dd8538648e05f7f1e9174b7fd6217cdf8786
edwinsebas <dexter417@hotmail.com>
2021-12-16 13:18:03 -0500

    initial commit

---
+++ src/main/java/calderon/edwin/anybank/AnybankApplication.java
@@ -0,0 +1,13 @@
+package calderon.edwin.anybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnybankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnybankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/calderon/edwin/anybank/AnybankApplicationTests.java
@@ -0,0 +1,13 @@
+package calderon.edwin.anybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnybankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
