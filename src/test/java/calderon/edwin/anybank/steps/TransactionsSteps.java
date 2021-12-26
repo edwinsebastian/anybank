656e25eccfbaa83c626729d5929dbd86b77b859a
edwinsebas <dexter417@hotmail.com>
2021-12-26 19:52:10 +0100

    add steps

--- src/test/java/calderon/edwin/anybank/steps/TransactionsSteps.java
+++ src/test/java/calderon/edwin/anybank/steps/TransactionsSteps.java
@@ -1,55 +1,104 @@
 package calderon.edwin.anybank.steps;
 
+//import calderon.edwin.anybank.model.TransactionModel;
+//import calderon.edwin.anybank.repository.TransactionRepository;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.UUID;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
-public class TransactionsSteps extends SpringIntegrationTest{
+public class TransactionsSteps extends SpringIntegrationTest {
 
-    @Given("^A transaction that is stored in our system$")
-    public void a_transaction_that_is_stored_in_our_system() {
-    }
+//    @Autowired
+//    private TransactionRepository transactionRepository;
+    private double amount;
+    private double fee;
+    private UUID transactionID;
 
     @Given("^A transaction that is not stored in our system$")
     public void a_transaction_that_is_not_stored_in_our_system() {
+        transactionID = UUID.randomUUID();
+    }
+
+    @Given("^A transaction that is stored in our system with transaction date (.+) than today$")
+    public void a_transaction_that_is_stored_in_our_system(String transactionDate) throws ParseException {
+        Date date;
+        transactionID = UUID.randomUUID();
+        switch (transactionDate)
+        {
+            case "before":
+                date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z")
+                        .parse(LocalDate.now(ZoneId.systemDefault()).minusDays(1).toString());
+                break;
+            case "greater":
+                date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z")
+                        .parse(LocalDate.now(ZoneId.systemDefault()).plusDays(1).toString());
+                break;
+            default:
+                date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z")
+                        .parse(LocalDate.now(ZoneId.systemDefault()).toString());
+                break;
+        }
+
+        amount = 89.1;
+        fee = 6.03;
+//        transactionRepository.save(new TransactionModel(transactionID,
+//                //UUID.fromString("98134c74bee24626aa52043ed92dde8c"), //TODO: add this parameter
+//                amount,
+//                fee,
+//                date,
+//                "Mousse - Chocolate"));
     }
 
     @When("^I check the status from (.+) channel$")
-    public void i_check_the_status_from_x_channel(String channel) {
+    public void i_check_the_status_from_x_channel(String channel) throws Throwable {
+        String path = "";
+        executeGet("http://localhost:8080" + path); //TODO call the real endpoint using channel and transactionID
+
+        HttpStatus currentStatusCode = latestResponse.getTheResponse().getStatusCode();
+        assertThat("status code is incorrect : " +
+                latestResponse.getBody(), currentStatusCode.value(), is(200));
     }
 
-    @And("^the transaction date is before today$")
-    public void the_transaction_date_is_before_today(){}
+    @Then("^The system returns the status (.+)$")
+    public void the_system_returns_the_status_X(String status) throws JSONException {
+        JSONObject response = new JSONObject(latestResponse.getTheResponse().toString());
 
-    @And("^the transaction date is equals to today$")
-    public void the_transaction_date_is_equals_to_today(){}
-
-    @And("^the transaction date is greater than today$")
-    public void the_transaction_date_is_greater_than_today(){}
-
-    @Then("^The system returns the status INVALID$")
-    public void the_system_returns_the_status_INVALID() {
-    }
-
-    @Then("^The system returns the status SETTLED$")
-    public void the_system_returns_the_status_SETTLED() {
-    }
-
-    @Then("^The system returns the status PENDING$")
-    public void the_system_returns_the_status_PENDING() {
-    }
-
-    @Then("^The system returns the status FUTURE$")
-    public void the_system_returns_the_status_FUTURE() {
+        assertThat("status transaction is incorrect : " +
+                response.getString("status").toUpperCase(), is(status.toUpperCase()));
     }
 
     @And("^the amount subtracting the fee$")
-    public void the_amount_subtracting_the_fee(){}
+    public void the_amount_subtracting_the_fee() throws JSONException {
+        JSONObject response = new JSONObject(latestResponse.getTheResponse().toString());
+
+        assertThat("amount incorrect : " + response.getString("amount"),
+                response.getString("amount"), is(amount - fee));
+    }
 
     @And("^the amount$")
-    public void the_amount(){}
+    public void the_amount() throws JSONException {
+        JSONObject response = new JSONObject(latestResponse.getTheResponse().toString());
+        assertThat("amount is incorrect : " + response.getString("amount"),
+                response.getString("amount"), is(amount));
+    }
 
     @And("^the fee$")
-    public void the_fee(){}
+    public void the_fee() throws JSONException {
+        JSONObject response = new JSONObject(latestResponse.getTheResponse().toString());
+        assertThat("Fee is incorrect : " + response.getString("fee"),
+                response.getString("fee"), is(fee));
+    }
 }
--- src/test/resources/transactions.feature
+++ src/test/resources/transactions.feature
@@ -1,58 +1,64 @@
-Feature: check transactions status
+Feature: Check transactions status
 
-  Scenario: A - check a not stored transaction from any channel
+  Scenario Outline: A - check a not stored transaction from any channel
     Given A transaction that is not stored in our system
-    When I check the status from any channel
+    When I check the status from <channel> channel
     Then The system returns the status INVALID
+    Examples:
+      |channel  |
+      |CLIENT   |
+      |ATM      |
+      |INTERNAL |
 
-  Scenario: B - check a stored transaction dated earlier than today from CLIENT or ATM channel
-    Given A transaction that is stored in our system
-    When I check the status from CLIENT or ATM channel
-    And the transaction date is before today
+  Scenario Outline: B - check a stored transaction dated earlier than today from CLIENT or ATM channel
+    Given A transaction that is stored in our system with transaction date before than today
+    When I check the status from <channel> channel
     Then The system returns the status SETTLED
     And the amount subtracting the fee
+    Examples:
+      |channel  |
+      |CLIENT   |
+      |ATM      |
 
   Scenario: C - check a stored transaction dated earlier than today from INTERNAL channel
-    Given A transaction that is stored in our system
+    Given A transaction that is stored in our system with transaction date before than today
     When I check the status from INTERNAL channel
-    And the transaction date is before today
     Then The system returns the status SETTLED
     And the amount
     And the fee
 
-  Scenario: D - check a stored transaction dated today from CLIENT or ATM channel
-    Given A transaction that is stored in our system
-    When I check the status from CLIENT or ATM channel
-    And the transaction date is equals to today
+  Scenario Outline: D - check a stored transaction dated today from CLIENT or ATM channel
+    Given A transaction that is stored in our system with transaction date equals than today
+    When I check the status from <channel> channel
     Then The system returns the status PENDING
     And the amount subtracting the fee
+    Examples:
+      |channel  |
+      |CLIENT   |
+      |ATM      |
 
   Scenario: E - check a stored transaction dated today from INTERNAL channel
-    Given A transaction that is stored in our system
+    Given A transaction that is stored in our system with transaction date equals than today
     When I check the status from INTERNAL channel
-    And the transaction date is equals to today
     Then The system returns the status PENDING
     And the amount
     And the fee
 
   Scenario: F - check a stored transaction date greater than today from CLIENT channel
-    Given A transaction that is stored in our system
+    Given A transaction that is stored in our system with transaction date greater than today
     When I check the status from CLIENT channel
-    And the transaction date is greater than today
     Then The system returns the status FUTURE
     And the amount subtracting the fee
 
   Scenario: G - check a stored transaction date greater than today from ATM channel
-    Given A transaction that is stored in our system
+    Given A transaction that is stored in our system with transaction date greater than today
     When I check the status from ATM channel
-    And the transaction date is greater than today
     Then The system returns the status PENDING
     And the amount subtracting the fee
 
   Scenario: H - check a stored transaction date greater than today from INTERNAL channel
-    Given A transaction that is stored in our system
+    Given A transaction that is stored in our system with transaction date greater than today
     When I check the status from INTERNAL channel
-    And the transaction date is greater than today
     Then The system returns the status FUTURE
     And the amount
     And the fee
